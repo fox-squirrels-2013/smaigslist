65edefbb73bc1ca8f868259700c9f3d1e08a362f
TGOlson <tydotg@gmail.com>
2013-11-04 17:57:21 -0800

    created table and migrations

---
+++ db/migrate/20131104175314_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.belongs_to :category
+      t.string     :title
+      t.string     :description
+      t.string     :price
+      t.string     :contact_email
+
+      t.timestamps
+    end
+  end
+end
